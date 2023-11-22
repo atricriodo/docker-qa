7fc7f9e34a708d56c3fd20b3e2e551a3f357782f
Saroj Kumar Mahato <sarojkumar@criodo.com>
2023-11-22 16:06:34 +0530

    cleanup

--- client.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-docker build . -t client -f ./Dockerfile-client
-docker run --rm --name client --network mynetwork client:latest
--- server.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-docker network create mynetwork
-docker build . -t hello-world-3 -f ./Dockerfile-server
-docker run -d --name server --network mynetwork -p 8080:8080 hello-world-3:latest
